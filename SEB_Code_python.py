db5fc89648c547dead52709ab254ec2c59563949
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-11-09 15:41:12 +0100

    Fix indent problems

--- SEB_Code_python.py
+++ SEB_Code_python.py
@@ -12,27 +12,23 @@ response = {}
 # def random_json() :
 count = 0
 numéro_unité = 1
-  while True:
-       for n in range(10):
-            num_auto = random.randint(1, 10)
-            type_auto = random.randint(1, 10)
-            temp_cuv = random.uniform(1, 10)
-            print(numéro_unité)
-            print(num_auto, type_auto, temp_cuv)
-
-            response["unite"] = num_auto
-            response["machine"] = type_auto
-            response["content"] = temp_cuv
-
-        str_response = json.dumps(response)
-
-        count += 1
-        donnees = str_response
-        with open("fichier%d.json" % (count), "w") as file:
-            json.dump(donnees, file)
-
-        print(str_response)
-        time.sleep(10)
+while True:
+    for n in range(10):
+         num_auto = random.randint(1, 10)
+         type_auto = random.randint(1, 10)
+         temp_cuv = random.uniform(1, 10)
+         print(numéro_unité)
+         print(num_auto, type_auto, temp_cuv)
+         response["unite"] = num_auto
+         response["machine"] = type_auto
+         response["content"] = temp_cuv
+    str_response = json.dumps(response)
+    count += 1
+    donnees = str_response
+    with open("fichier%d.json" % (count), "w") as file:
+        json.dump(donnees, file)
+    print(str_response)
+    time.sleep(10)
 
 
 # Création d'un json
