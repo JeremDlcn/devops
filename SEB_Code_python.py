9eab8ee177dbd94e20c11b1a5a51d6277db9ff43
sebastienplottu <sebastien.plottu@ynov.com>
2020-11-09 14:09:39 +0100

    Update SEB_Code_python.py

--- SEB_Code_python.py
+++ SEB_Code_python.py
@@ -1,6 +1,26 @@
+import random
 
 
-# Créez un script qui créeer un json et l'envoie au controlleur (envoie de données via les sockets dockers)
+# Créez un script qui créer un json et l'envoie au controlleur (envoie de données via les sockets dockers)
+
+# Génération  de code aléatoire
+
+# def random_json() :
+
+numéro_unité = 1
+
+for n in range(10):
+    num_auto = random.randint(1, 10)
+    type_auto = random.randint(1, 10)
+    temp_cuv = random.randint(1, 10)
+    print(numéro_unité)
+    print(num_auto, type_auto, temp_cuv)
+
+
+# Création d'un json
+
+
+# envoie automatique du json
 
 
 # Socket Python
