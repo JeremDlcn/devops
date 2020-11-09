651ade75a0518f6c951a859b029222bb05d71a23
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-11-09 14:07:32 +0100

    Add create_json_file function

--- SEB_Code_python.py
+++ SEB_Code_python.py
@@ -1,6 +1,17 @@
-
-
 # Créez un script qui créeer un json et l'envoie au controlleur (envoie de données via les sockets dockers)
+import json
 
+response = {}
+
+response["unite"] = 5
+response["machine"] = "Lait"
+response["content"] = 5.5
+
+str_response = json.dumps(response)
+print(str_response)
+
+donnees = str_response
+with open("C:/Users/Public/DevOps/devops/fichier.json", "w") as file:
+    json.dump(donnees, file)
 
 # Socket Python
