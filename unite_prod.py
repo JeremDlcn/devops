f0fdcaa20628118847224046b5bebe50759f9839
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-11-10 16:32:18 +0100

    New file random content

---
+++ unite_prod.py
@@ -0,0 +1,36 @@
+import random
+import json
+import time
+from datetime import datetime
+
+# Créez un script qui créer un json et l'envoie au controlleur (envoie de données via les sockets dockers)
+def random_json(numero_unite) :
+    num_machine = 1
+    
+    # Génération  de code aléatoire
+    for n in range(10):
+        dt = int(time.time())
+        num_auto = numero_unite
+        type_auto = num_machine
+        temp_cuv = random.uniform(1, 10)
+        personneDict = {"emp_details": [{ "unite": num_auto, "machine":type_auto, "type_auto": temp_cuv }]}
+        with open("JSON_Files/paramunite_%d_%d_%d.json" % (numero_unite, num_machine, dt), "w") as jsonFile:
+            json.dump(personneDict, jsonFile)
+        num_machine+=1
+    return("Exports reussis")
+
+
+while True:
+    #Get Unix date time format
+    
+
+    for x in range(1,6):
+        random_json(x)
+    
+    time.sleep(60)
+
+# Création d'un json
+
+# envoie automatique du json
+
+# Socket Python
