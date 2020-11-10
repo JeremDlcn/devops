c889f1fe52bfdce3e8882f4fd58b959a64c7da77
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-11-10 22:18:13 +0100

    🗃 update select query with peewee

--- devops/models.py
+++ devops/models.py
@@ -14,20 +14,25 @@ class BaseModel(Model):
 
 
 
-class Facility(BaseModel):
-    id = IntegerField(primary_key=True)
-    temperature = IntegerField()
-    nbMachine = IntegerField()
-    name = CharField()
+class Unit(BaseModel):
+    numero_unite = IntegerField(primary_key=True)
+    nom_unite = CharField()
 
     class Meta:
-        table_name = 'automate'
+        table_name = 'unites'
+
 
 def init_db():
 	db.connect()
 
 def value():
-    print(Facility.select())
+    # only one row
+    # d = Unit.get(Unit.numero_unite==2)
+    # print(d.numero_unite, d.nom_unite)
+    # multiple rows
+    f = Unit.select()
+    for cf in f:     
+        print(cf.numero_unite, cf.nom_unite)
 
 def close():
     db.close()
--- devops/static/js/app.js
+++ devops/static/js/app.js
@@ -2,12 +2,41 @@ let ctx = document.getElementById('chart').getContext('2d');
 
 let lineChart = new Chart(ctx, {
     type: 'line',
-    data: [{
-        x: 10,
-        y: 20
-    }, {
-        x: 15,
-        y: 10
-    }],
-    // options: options
+    data: {
+        labels: ["00", "10", "20", "30", "40", "50", "60"],
+        datasets: [
+            {
+                label: 'Montagne russe',
+                borderColor: "rgba(78, 115, 223, 1)",
+                data: [2.5, 2.5, 2.8, 2.0, 3.0, 2.8, 4.0],
+            },
+            {
+                label: 'Montagne Française',
+                borderColor: "rgba(255, 75, 103, 1)",
+                data: [2.2, 2.0, 1.8, 2.8, 3.9, 1.0, 2.0],
+            },
+        ]
+    }, 
+    options: {
+        scales: {
+            xAxes: [
+                {
+                    position: "botom",
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Frequency (Hz)"
+                    }
+                }
+            ],
+            yAxes: [
+                {
+                    position: "top",
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Frequency (Hz)",
+                    }
+                }
+            ],
+        }
+    }
 });
