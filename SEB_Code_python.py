4595866d9ded955242401bfc8fd33316f5f76899
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-11-10 08:56:12 +0100

    New JSON file format

--- SEB_Code_python.py
+++ SEB_Code_python.py
@@ -1,9 +1,6 @@
 import random
-# Créez un script qui créeer un json et l'envoie au controlleur (envoie de données via les sockets dockers)
 import json
 import time
-
-response = {}
 
 # Créez un script qui créer un json et l'envoie au controlleur (envoie de données via les sockets dockers)
 
@@ -19,20 +16,27 @@ while True:
          temp_cuv = random.uniform(1, 10)
          print(numéro_unité)
          print(num_auto, type_auto, temp_cuv)
-         response["unite"] = num_auto
-         response["machine"] = type_auto
-         response["content"] = temp_cuv
-    str_response = json.dumps(response)
+         
+    personneDict = "{'emp_details': [{ 'unite': num_auto, 'machine':type_auto, 'type_auto': type_auto }]}"
     count += 1
-    donnees = str_response
-    with open("JSON_Files/fichier%d.json" % (count), "w") as file:
-        json.dump(donnees, file)
-    print(str_response)
+    with open("JSON_Files/fichier%d.json" % (count), "w") as jsonFile:
+        json.dump(personneDict, jsonFile)
+
     time.sleep(10)
 
 
 # Création d'un json
-
+"""
+response["unite"] = num_auto
+response["machine"] = type_auto
+response["type_auto"] = type_auto
+str_response = json.dumps(response)
+count += 1
+donnees = str_response
+with open("JSON_Files/fichier%d.json" % (count), "w") as file:
+    json.dump(donnees, file)
+print(str_response)
+"""
 
 # envoie automatique du json
 
