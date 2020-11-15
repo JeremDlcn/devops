20b675acaa6c27078cae309ec665f45833c25d04
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-11-15 23:37:10 +0100

    🚧 adding pagination button
    Pagination button allow the user to choose between unit in unit view

--- devops/static/js/view.js
+++ devops/static/js/view.js
@@ -1,10 +1,13 @@
 const viewGlobal = document.querySelector('.view-global');
 const viewUnit = document.querySelector('.view-unit');
+const pagination = document.querySelector('.pagination');
 
 viewGlobal.addEventListener('click', ()=>{
     viewGlobal.parentElement.classList.remove('active')
+    pagination.classList.remove('exist');
 });
 
 viewUnit.addEventListener('click', ()=>{
     viewUnit.parentElement.classList.add('active')
+    pagination.classList.add('exist');
 });
