bc9ee9a18d337a14577d7cb223b3ae7bc3cadf86
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-11-17 14:15:54 +0100

    Add socket client and server

---
+++ socket_client.py
@@ -0,0 +1,35 @@
+import socket
+import random
+import json
+import time
+from datetime import datetime
+
+hote = "localhost"
+port = 2222
+
+connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_avec_serveur.connect((hote, port))
+print("Connexion établie avec le serveur sur le port {}".format(port))
+msg_a_envoyer = b""
+while msg_a_envoyer != b"fin":
+        
+    num_machine = 1
+    # Génération  de code aléatoire
+    for n in range(10):
+        dt = int(time.time())
+        num_auto = 1
+        type_auto = num_machine
+        temp_cuv = random.uniform(1, 10)
+        personneDict = {"emp_details": [{ "unite": num_auto, "machine":type_auto, "type_auto": temp_cuv }]}
+        print(type(personneDict))
+        with open("JSON_Files/paramunite_%d_%d_%d.json" % (num_auto, num_machine, dt), "w") as jsonFile:
+            json.dump(personneDict, jsonFile)
+        num_machine+=1
+        msg_a_envoyer = str(personneDict)
+        msg_a_envoyer = msg_a_envoyer.encode()
+        connexion_avec_serveur.send(msg_a_envoyer)
+        time.sleep(10)
+    time.sleep(60)
+
+print("Fermeture de la connexion")
+connexion_avec_serveur.close()
