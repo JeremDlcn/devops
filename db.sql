90d5f0c161a697ec2d48c97bb04ea9aefd061601
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2020-11-21 19:44:34 +0100

    Adding script for database

---
+++ db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `measures` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `unitNumber` int(11) NOT NULL,
+  `robotNumber` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `tankTemp` double(8,2) NOT NULL,
+  `extTemp` double(8,2) NOT NULL,
+  `tankWeight` double(8,2) NOT NULL,
+  `productWeight` double(8,2) NOT NULL,
+  `ph` double(8,2) NOT NULL,
+  `kPlus` int(11) NOT NULL,
+  `nacl` double(8,2) NOT NULL,
+  `salmonellaLevel` int(11) NOT NULL,
+  `ecoliLevel` int(11) NOT NULL,
+  `listeriaLevel` int(11) NOT NULL,
+  `timeStamp` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `measures`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `measures`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
