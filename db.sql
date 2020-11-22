b5ab54748a8b1b55150a59ed566ffbe47ae55529
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2020-11-22 14:48:57 +0100

    new sript for database

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ CREATE TABLE `measures` (
   `id` int(10) UNSIGNED NOT NULL,
   `unitNumber` int(11) NOT NULL,
   `robotNumber` int(11) NOT NULL,
-  `type` varchar(255) NOT NULL,
+  `robotType` varchar(255) NOT NULL,
   `tankTemp` double(8,2) NOT NULL,
   `extTemp` double(8,2) NOT NULL,
   `tankWeight` double(8,2) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE `measures` (
   `salmonellaLevel` int(11) NOT NULL,
   `ecoliLevel` int(11) NOT NULL,
   `listeriaLevel` int(11) NOT NULL,
-  `timeStamp` varchar(11) NOT NULL
+  `timeStamp` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `measures`
