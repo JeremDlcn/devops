838986ccabf2b8881a25c312eeeacf6d5ec874c3
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2020-11-24 19:47:44 +0100

    Update Test_unitaire_SEB.py

--- Test_unitaire_SEB.py
+++ Test_unitaire_SEB.py
@@ -9,7 +9,7 @@ class RandomTest(unittest.TestCase):
     """Test case utilise pour tester les fonctions du module 'random'."""
 
     def test_choice(self):
-        """Test le fonctionnement de la fonction 'random.choice'."""
+        """Test le fonctionnement"""
         liste = list(range(10))
         elt = random.choice(liste)
         # Vérifie que 'elt' est dans 'liste'
