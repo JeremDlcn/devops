bc4ed46d3459d15aa7df96768e5d83dd272c3ad3
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2020-11-27 22:11:52 +0100

    Update test_templates.py

--- test_templates.py
+++ test_templates.py
@@ -2,7 +2,7 @@
 import unittest
 import os.path
 
-# Création de la class contenant les tests
+# Creation de la class contenant les tests
 class TempTest(unittest.TestCase):
 
     def test_main_file(self):
@@ -13,20 +13,20 @@ class TempTest(unittest.TestCase):
         self.assertTrue(os.path.isfile('./main.py'))
 
     def test_models_file(self):
-        """ Test de l'existance du fichier models.py
+        """ Test sur le fichier models.py
 
         models.py contient le modèle des tables de la base de données
         """
         self.assertTrue(os.path.isfile('./models.py'))
 
     def test_template(self):
-        """Test de l'existance des templates HTML
-        Les templates HTML sont les vues gérer par du Javascript
+        """Test sur les templates HTML
+        Les templates HTML sont les vues gerer par du Javascript
         """
         self.assertTrue(os.path.isfile('./templates/index.html'))
 
 
 # Lancement des tests uniquement si le fichier 
-# est directement appelé par ligne de commande
+# est directement appele par ligne de commande
 if __name__ == "__main__":
     unittest.main()
