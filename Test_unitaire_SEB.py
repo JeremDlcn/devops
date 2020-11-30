accb6586e335330d2ea0e5cd287fe8b9896d688a
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-11-30 09:46:20 +0100

    Replace Seb test

---
+++ Test_unitaire_SEB.py
@@ -0,0 +1,19 @@
+# --coding:Latin-1 -
+
+import random
+import unittest
+
+
+class RandomTest(unittest.TestCase):
+
+    """Test case utilise pour tester les fonctions du module 'random'."""
+
+    def test_choice(self):
+        """Test le fonctionnement de la fonction 'random.choice'."""
+        liste = list(range(10))
+        elt = random.choice(liste)
+        # Vérifie que 'elt' est dans 'liste'
+        self.assertIn(elt, liste)
+
+
+unittest.main()
